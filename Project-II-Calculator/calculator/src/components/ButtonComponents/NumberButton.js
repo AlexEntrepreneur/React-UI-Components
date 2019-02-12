1eddc38882766178ae4ccef4a25500c077983474
Alex Entrepreneur <hello@alexentrepreneur.com>
2019-02-12 13:13:46 +0000

    Create Calculator Component

--- Project-II-Calculator/calculator/src/components/ButtonComponents/NumberButton.js
+++ Project-II-Calculator/calculator/src/components/ButtonComponents/NumberButton.js
@@ -1,2 +1,16 @@
 import React from 'react';
 import './Button.css';
+
+function NumberButton({ buttonStyle, numbersArray }) {
+  return (
+    <div className="number-buttons-container">
+      {
+        numbersArray.map(number => {
+          return <button className={buttonStyle} key={number}>{number}</button>;
+        })
+      }
+    </div>
+  );
+}
+
+export default NumberButton;
