e2dbbdcf3bf496cc5c33fca4207554db7ecca76c
Alex Entrepreneur <hello@alexentrepreneur.com>
2019-02-12 00:40:19 +0000

    Create Card Header Components

--- Project-I-Social-Card/social-card/src/components/HeaderComponents/ImageThumbnail.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/ImageThumbnail.js
@@ -1,2 +1,15 @@
 import React from 'react';
 import './Header.css';
+
+const ImageThumbnail = ({ avatarImgSrc, avatarAltText  }) => {
+  return (
+    <div className="card-avatar">
+      <img className="card-avatar-img"
+        src={ avatarImgSrc }
+        alt={ avatarAltText }
+      />
+  </div>
+  );
+}
+
+export default ImageThumbnail;
