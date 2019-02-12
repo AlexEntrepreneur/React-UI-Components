6f9e03df1d33b7bb54a37bfd77ccee13dad6b0a0
Alex Entrepreneur <hello@alexentrepreneur.com>
2019-02-12 01:12:34 +0000

    Create Card Content Components

--- Project-I-Social-Card/social-card/src/components/CardComponents/CardBanner.js
+++ Project-I-Social-Card/social-card/src/components/CardComponents/CardBanner.js
@@ -1,2 +1,10 @@
 import React from 'react';
 import './Card.css';
+
+const CardBanner = ({ bannerImgSrc, bannerImgAlt }) => {
+  return (
+    <img src={ bannerImgSrc } alt={ bannerImgAlt }/>
+  );
+}
+
+export default CardBanner;
--- Project-I-Social-Card/social-card/src/components/CardComponents/CardContainer.js
+++ Project-I-Social-Card/social-card/src/components/CardComponents/CardContainer.js
@@ -1,2 +1,17 @@
 import React from 'react';
 import './Card.css';
+import CardContent from './CardContent.js';
+
+const CardContainer = () => {
+  return (
+    <div className="card-body-container">
+      <CardContent
+        cardContentTitle="Get Started with React"
+        cardContentBodyText="Something Nice"
+        cardContentLink="reactjs.org"
+      />
+    </div>
+  );
+}
+
+export default CardContainer;
